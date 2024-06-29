881056e2515cc3ce077a236f82d20519370ecebf
shaliniyuvraj <shaliniyuvraj1127@gmail.com>
2024-06-29 13:56:54 +0530

    added js file

---
+++ java.js
@@ -0,0 +1,51 @@
+var typed = new Typed('#element', {
+    strings: ['Web Devlopers','Web Designers'],
+    typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+  loopCount: Infinity,
+  showCursor: true,
+  cursorChar: '|',
+  });
+
+
+  var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+  for(tablink of tablinks){
+     tablink.classList.remove("active-link");
+  }
+  for(tabcontent of tabcontents){
+      tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+ 
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyUl3kqkudYs76wEvemXdLIgczDEwF4whN5OZENrAW5y_IdMqUafbeBxQ-Xuuncn_8m/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response =>{
+        msg.innerHTML ="Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
